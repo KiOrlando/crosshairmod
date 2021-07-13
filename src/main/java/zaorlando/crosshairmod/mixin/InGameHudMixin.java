9cebb9a71f6c431f0feb8ac92e8db29269a25880
ZaOrlando <70098517+ZaOrlando@users.noreply.github.com>
2021-07-13 11:41:13 -0400

    Added more crosshairs and made the code work!

--- src/main/java/zaorlando/crosshairmod/mixin/InGameHudMixin.java
+++ src/main/java/zaorlando/crosshairmod/mixin/InGameHudMixin.java
@@ -5,12 +5,11 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import zaorlando.crosshairmod.CrosshairMod;
 
 @Mixin(InGameHud.class)
@@ -20,6 +19,8 @@ public class InGameHudMixin
     private static final Identifier GUI_CROSSHAIR1_TEXTURE = new Identifier(CrosshairMod.MOD_ID, "textures/gui/crosshair1.png");
     // Looking at block
     private static final Identifier GUI_CROSSHAIR2_TEXTURE = new Identifier(CrosshairMod.MOD_ID, "textures/gui/crosshair2.png");
+    // Looking at block that cant be harvested
+    private static final Identifier GUI_CROSSHAIR4_TEXTURE = new Identifier(CrosshairMod.MOD_ID, "textures/gui/crosshair4.png");
     // Looking at nothing
     private static final Identifier GUI_CROSSHAIR3_TEXTURE = new Identifier(CrosshairMod.MOD_ID, "textures/gui/crosshair3.png");
 
@@ -29,44 +30,50 @@ public class InGameHudMixin
     @Shadow
     private int scaledHeight;
 
-    @Inject(locals = LocalCapture.CAPTURE_FAILHARD,
-            method="renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
-            at = @At(value = "INVOKE",
-                     shift = At.Shift.BEFORE,
-                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
-    private void renderCrosshairMixinBefore(MatrixStack ms, CallbackInfo ci)
+    @Redirect(method = "renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
+              at = @At(value = "INVOKE",
+                       target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V",
+                       ordinal = 0))
+    private void crosshairmod_renderCrosshairRedirect(InGameHud inGameHud, MatrixStack matrices, int x, int y, int u, int v, int width, int height)
     {
-        if(MinecraftClient.getInstance().crosshairTarget != null)
+        var client = MinecraftClient.getInstance();
+
+        if(client.crosshairTarget != null && client.world != null && client.player != null)
         {
-            switch(MinecraftClient.getInstance().crosshairTarget.getType())
+            switch(client.crosshairTarget.getType())
             {
                 // Looking at entity
                 case ENTITY -> {
+                    // var hit = ((EntityHitResult)client.crosshairTarget);
+                    // var entity = client.targetedEntity;
                     RenderSystem.setShaderTexture(0, GUI_CROSSHAIR1_TEXTURE);
                 }
                 // Looking at block
                 case BLOCK -> {
-                    RenderSystem.setShaderTexture(0, GUI_CROSSHAIR2_TEXTURE);
+                    var hit = ((BlockHitResult)client.crosshairTarget);
+
+                    // Check is player can collect the block they are looking at.
+                    if(client.player.isCreative() || client.player.canHarvest(client.world.getBlockState(hit.getBlockPos())))
+                        RenderSystem.setShaderTexture(0, GUI_CROSSHAIR2_TEXTURE);
+                    else
+                        RenderSystem.setShaderTexture(0, GUI_CROSSHAIR4_TEXTURE);
                 }
                 // Looking at nothing
                 default -> {
+                    var hit = client.crosshairTarget;
                     RenderSystem.setShaderTexture(0, GUI_CROSSHAIR3_TEXTURE);
                 }
             }
         }
-        else
-        {
+        else if(client.crosshairTarget == null)
             CrosshairMod.LOGGER.error("Could not change crosshair because the 'MinecraftClient.getInstance().crosshairTarget' object is null.");
-        }
-        InGameHud.drawTexture(ms, (this.scaledWidth - 15) / 2, (this.scaledHeight - 15) / 2, 0, 0, 15, 15, 15, 15);
-    }
+        else if(client.world == null)
+            CrosshairMod.LOGGER.error("Could not change crosshair because the 'MinecraftClient.getInstance().world' object is null.");
+        else
+            CrosshairMod.LOGGER.error("Could not change crosshair because the 'MinecraftClient.getInstance().player' object is null.");
 
-    @Inject(method = "renderCrosshair(Lnet/minecraft/client/util/math/MatrixStack;)V",
-            at = @At(value = "INVOKE",
-                     shift = At.Shift.AFTER,
-                     target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
-    private void renderCrosshairMixinAfter(MatrixStack ms, CallbackInfo ci)
-    {
+        InGameHud.drawTexture(matrices, (this.scaledWidth - 15) / 2, (this.scaledHeight - 15) / 2, 0, 0, 15, 15, 15, 15);
+
         RenderSystem.setShaderTexture(0, InGameHud.GUI_ICONS_TEXTURE);
     }
 }
